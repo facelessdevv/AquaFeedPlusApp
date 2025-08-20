5e2e45d89587a0275d6e6eaf769aa902468f9823
facelessdevv <dev.faceless.contat@gmail.om>
2025-08-20 23:20:31 +0330

    - Fix LTR to RTL

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import AppStack from './src/navigation/AppStack';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { CartProvider } from './src/context/CartContext';
+import { I18nManager } from "react-native";
+
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
 
 const App = () => {
   return (
